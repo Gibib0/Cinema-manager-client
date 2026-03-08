ccc24c7e13bed77eb7fc6bcdb69e69e3fcaf4fee
Anastasia <anast111antonova@gmail.com>
2026-03-09 00:31:30 +0100

    Add services and carousel

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,45 @@
+import Carousel from 'react-material-ui-carousel'
+import Box from '@mui/material/Box'
+import { posters } from "../../constants"
+
+const styles = {
+	imgContainerStyle: {
+		position: 'relative',
+		maxWidth: '100%',
+		height: '70vh',
+		overflow: 'hidden',
+		color: 'red'
+	},
+	imgStyle: {
+		borderRadius: '15px',
+		padding: '5px',
+		maxWidth: '100%',
+		maxHeight: '100%',
+		position: 'absolute',
+		left: '50%',
+		transform: 'translate{-50%, 0}',
+		objectFit: 'contain'
+	}
+}
+
+function HomePage() {
+	return (
+		<>
+			<Carousel>
+				{posters.map((poster) => {
+					return (
+						<Box key={poster.id} style={styles.imgContainerStyle}>
+							<img 
+								src={poster.url}
+								alt={poster.alt}
+								style={styles.imgStyle}
+								/>
+						</Box>
+					)
+				})}
+			</Carousel>
+		</>
+	)
+}
+
+export default HomePage
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const posters = [
+	{id: 1, url: 'https://pluggedin.ru/images/upload/1655926482.jpg', alt: 'poster'},
+	{id: 2, url: 'https: //wwwcookbook.ru/imgrab/0068/978541165246.jpd', alt: 'poster'},
+	{id: 3, url: 'https://themoviedb.org/t/p/w600_and_h900_bestv2/6tDkZyqzZVQpLqYgT8iXj9rHkP.jpg', alt: 'poster'},
+	{id: 4, url: 'https://xage.ru/images/upload/1655926482.jpg', alt: 'poster'},
+	{id: 5, url: 'https://poster4.me/wp-content/uploads/2020/11/poster4me-5fbf1c8e7b9e2.jpg', alt: 'poster'},
+	{id: 6, url: 'https://xage.ru/images/upload/1655926482.jpg', alt: 'poster'},
+	{id: 7, url: 'https://megaobzor.com/wp-content/uploads/2020/11/poster4me-5fbf1c8e7b9e2.jpg', alt: 'poster'},
+]
